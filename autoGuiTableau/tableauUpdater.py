1167c9e0bdc6a238f55947ef525b0274d710ea3b
nstark <reynz11@gmail.com>
2017-04-19 16:27:13 -0400

    Further email work

--- autoGuiTableau/tableauUpdater.py
+++ autoGuiTableau/tableauUpdater.py
@@ -15,6 +15,10 @@ import subprocess
 import logging
 import sys
 import smtplib
+from email.mime import multipart
+from email.mime import text
+from email.mime import base
+from email import encoders
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s.%(message)03d: %(message)s', datefmt='%H:%M:%S')
 # logging.disable(logging.info) # UNCOMMENT TO BLOCK DEBUG LOG MESSAGES
@@ -28,7 +32,7 @@ reportName = 'Scrap.twb'
 def main():
     '''
     Runs the entire program. 
-    :return: End result of program: Saved .twbx and an email sent with updated .twbx
+    :return: End result of program: Saved .twbx and an email sent with updated .twbx file attached
     '''
     logging.info('Program started. Press Ctrl-C to abort at any time.')
     logging.info('To pause mouse movement, move cursor to the very top left of the screen.')
@@ -112,38 +116,57 @@ def saveAsPackagedWorkbook():
 def sendEmail():
     '''
     Sends an email with the updated .twbx file, able to be opened in Tableau Reader
-    :param twbxFilename: Filename of the packaged Tableau to be mailed out
-    :param subject: Subject of the email
-    :return: Email object to send out
     '''
 
+    logging.info('Building email message...')
+
     # CREDENTIALS
-    outlook_user = 'nstark@bradfordwhite.com'
-    # outlook_pwd = ''                                  # CAN HARDCOdE THIS OR USE sys.argv[] AND YOU'LL BE PROMPTED
+    fromaddr = 'nstark@bradfordwhite.com'               # YOUR EMAIL
+    toaddr = 'nstark@bradfordwhite.com'                 # EMAIL ADDRESS YOU SEND TO
+    # outlook_pwd = ''                                  # CAN HARDCODE THIS OR USE sys.argv[] AND YOU'LL BE PROMPTED
 
     # MESSAGE INFORMATION
-    sender = 'nstark@bradfordwhite.com'
-    recipient = 'nstark@bradfordwhite.com'
-    header = 'To:' + recipient + '\n' + 'From: ' + outlook_user + '\n' + 'Subject: testing \n'
-    message = header + '\n Testing. This message is not real. \n\n'
+    msg = multipart()
 
-    logging.info('Sending email to: ' + recipient + '.\nFrom: ' + outlook_user)
+    msg['From'] = fromaddr
+    msg['To'] = toaddr
+    msg['Subject'] = 'TEST'                            # SUBJECT OF EMAIL
+
+    body = "TESTING THE BODY OF THE EMAIL"             # TEXT YOU WANT TO SEND
+
+    msg.attach(text(body, 'plain'))
+
+    # EMAIL ATTACHMENT INFO
+    filename = 'Scrap.twbx'
+    attachment = open(r'C:\Users\NStark\Desktop\Nick Stark\Tableau\Scrap\Scrap.twbx', 'rb')
+
+    # ADD ATTACHMENT TO HEADER OF EMAIL
+    part = base('application', 'octet-stream')
+    part.set_payload((attachment).read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+
+    msg.attach(part)
+
+    logging.info('Sending email to: ' + toaddr + '.\nFrom: ' + fromaddr)
 
      # CONNECT AND SEND MAIL
-    smtpserver = smtplib.SMTP('w2k8e-exch.grr.bradfordwhite.com', 587)        # PORT MAY ALSO BE 995
+    smtpserver = smtplib.SMTP('w2k8e-exch.grr.bradfordwhite.com', 587)          # PORT MAY BE WRONG
     smtpserver.ehlo()
     smtpserver.starttls()
-    # smtpserver.login(outlook_user, outlook_pwd)                             # UNCOMMENT FOR HARDCODED PWD STORED
-    smtpserver.login(outlook_user, sys.argv[1])
-    sendMailStatus = smtpserver.sendmail(from_addr=sender, to_addrs=recipient, msg=message)
+    # smtpserver.login(fromaddr, outlook_pwd)                                   # UNCOMMENT FOR HARDCODED PWD STORED
+    smtpserver.login(fromaddr, sys.argv[1])
+    sendMailStatus = smtpserver.sendmail(from_addr=fromaddr, to_addrs=toaddr, msg=msg)
 
 
     # NOTIFY IF ERROR
     if sendMailStatus != {}:
-        print('There was a problem sending an email to %s: %s' % (recipient, sendMailStatus))
+        print('There was a problem sending an email to %s: %s' % (toaddr, sendMailStatus))
 
 
+# CALL MAIN
 if __name__ == '__main__':
     main()
 
+# EXIT PROGRAM UPON COMPLETION OF MAIN FUNCTION
 sys.exit("Exiting program.")
